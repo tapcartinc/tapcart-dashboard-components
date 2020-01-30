e54c34b39e1f72d403a9f8969b794b58d02d6c2f
alexiastandish <alexiastandish@gmail.com>
2020-01-30 13:02:00 -0800

    removed unused components in dash

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,13 +1,10 @@
 export * from "./Button/index.stories";
 export * from "./Typography/index.stories";
-// export * from "./Accordion/index.stories";
 export * from "./Input/index.stories";
 export * from "./Pagination/index.stories";
-export * from "./ThemeProvider/index.stories";
 export * from "./Modal/index.stories";
 export * from "./SegmentedControl/index.stories";
 export * from "./ClickDropdown/index.stories";
-export * from "./ContainerBox/index.stories";
 export * from "./HoverDropdown/index.stories";
 export * from "./ToolTip/index.stories";
 export * from "./Card/index.stories";
