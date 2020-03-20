56956e8346197ec9dc71a81ab73400f788a1ab38
alexiastandish <alexiastandish@gmail.com>
2020-03-20 15:34:03 -0700

    blahhh

--- build/stats/charts/AreaGraph/index.js
+++ build/stats/charts/AreaGraph/index.js
@@ -27,20 +27,6 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
-
-function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
-
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
@@ -49,27 +35,46 @@ function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) ||
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
 function convert(d) {
   var date = (0, _moment["default"])(d).toDate();
   return date;
-} // const convertData = data => {
-//   return data.reduce((acc, dataSet, index) => {
-//     if (dataSet.data.length) {
-//       return [
-//         ...acc,
-//         {
-//           key: dataSet.key,
-//           data: dataSet.data.map((key, index) => {
-//             return (key[index] = { ...key, key: convert(key.key) });
-//           })
-//         }
-//       ];
-//     } else {
-//       return [...acc, { ...dataSet, key: convert(dataSet.key) }];
-//     }
-//   }, []);
-// };
+}
 
+var convertData = function convertData(data) {
+  return data && data.reduce(function (acc, dataSet, index) {
+    console.log("dataSet", dataSet);
+    console.log("acc", acc);
+
+    if (dataSet.data.length) {
+      return [].concat(_toConsumableArray(acc), [{
+        key: dataSet.key,
+        data: dataSet.data.map(function (key, index) {
+          return key[index] = _objectSpread({}, key, {
+            key: convert(key.key)
+          });
+        })
+      }]);
+    } else {
+      return [].concat(_toConsumableArray(acc), [_objectSpread({}, dataSet, {
+        key: convert(dataSet.key)
+      })]);
+    }
+  }, []);
+};
 
 var AreaGraph = function AreaGraph(props) {
   var _useState = (0, _react.useState)({
@@ -82,6 +87,7 @@ var AreaGraph = function AreaGraph(props) {
       state = _useState2[0],
       setState = _useState2[1];
 
+  console.log("state.data && state.data", state.data && state.data);
   var currentData = (0, _react.useRef)();
   (0, _react.useEffect)(function () {
     currentData.current = state.data;
@@ -89,8 +95,7 @@ var AreaGraph = function AreaGraph(props) {
     if (!previousData && props.data) {
       return setState(function (prevState) {
         return _objectSpread({}, prevState, {
-          // data: convertData(props.data)
-          data: props.data
+          data: convertData(props.data)
         });
       });
     }
