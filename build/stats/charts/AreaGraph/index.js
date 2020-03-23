403d2a646edeb11824dce566d1157f2055d359bf
alexiastandish <alexiastandish@gmail.com>
2020-03-23 15:36:54 -0700

    fixing data passing

--- build/stats/charts/AreaGraph/index.js
+++ build/stats/charts/AreaGraph/index.js
@@ -27,14 +27,6 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
-
-function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
@@ -74,64 +66,55 @@ var convertData = function convertData(data) {
 };
 
 var AreaGraph = function AreaGraph(props) {
-  console.log("props", props);
+  console.log("props", props); // const [state, setState] = useState({
+  //   data: convertData(props.data),
+  //   dimmension: "",
+  //   prevData: [],
+  //   range: []
+  // });
 
-  var _useState = (0, _react.useState)({
-    data: [],
-    dimmension: "",
-    prevData: [],
-    range: []
-  }),
-      _useState2 = _slicedToArray(_useState, 2),
-      state = _useState2[0],
-      setState = _useState2[1];
+  var updatedData = props.data && convertData(props.data);
+  var currentData = (0, _react.useRef)(); // useEffect(() => {
+  //   currentData.current = state.data;
+  //   if (!previousData && props.data) {
+  //     return setState(prevState => ({
+  //       ...prevState,
+  //       data: convertData(props.data)
+  //     }));
+  //   }
+  //   // if (state.range.length <= 0 && state.data.length > 0) {
+  //   //   setRange();
+  //   // }
+  // }, [props.data, state.data]);
 
-  var currentData = (0, _react.useRef)();
-  (0, _react.useEffect)(function () {
-    currentData.current = state.data;
-
-    if (!previousData && props.data) {
-      return setState(function (prevState) {
-        return _objectSpread({}, prevState, {
-          data: convertData(props.data)
-        });
-      });
-    }
-
-    if (state.range.length <= 0 && state.data.length > 0) {
-      setRange();
-    }
-  }, [props.data, state.data]);
   var previousData = currentData.current;
   var stacked = props.stacked,
       currency = props.currency,
       colors = props.colors,
       tooltip = props.tooltip,
-      statValues = props.statValues,
       range = props.range,
       gradient = props.gradient,
       fillColors = props.fillColors,
       info = props.info,
       body = props.body,
-      removeCard = props.removeCard;
-
-  var setRange = function setRange() {
-    state.data.map(function (dataSet, index) {
-      if (dataSet.data.length) {
-        return setState(function (prevState) {
-          return _objectSpread({}, prevState, {
-            range: [dataSet.data[0].key, dataSet.data[dataSet.data.length - 1].key]
-          });
-        });
-      } else {
-        return setState(function (prevState) {
-          return _objectSpread({}, prevState, {
-            range: [state.data[0], state.data[state.data.length - 1]]
-          });
-        });
-      }
-    });
-  };
+      removeCard = props.removeCard; // const setRange = () => {
+  //   state.data.map((dataSet, index) => {
+  //     if (dataSet.data.length) {
+  //       return setState(prevState => ({
+  //         ...prevState,
+  //         range: [
+  //           dataSet.data[0].key,
+  //           dataSet.data[dataSet.data.length - 1].key
+  //         ]
+  //       }));
+  //     } else {
+  //       return setState(prevState => ({
+  //         ...prevState,
+  //         range: [state.data[0], state.data[state.data.length - 1]]
+  //       }));
+  //     }
+  //   });
+  // };
 
   var getColorScheme = function getColorScheme() {
     if (colors) {
@@ -174,9 +157,10 @@ var AreaGraph = function AreaGraph(props) {
       className: "arrow"
     }, "\u2193"), currency && _react["default"].createElement("span", null, currency, " "), Number(statInfo.previousTotal).toLocaleString(), " (", statInfo.percentChange, "%)"));
   })), _react["default"].createElement(GraphDetails, {
+    data: updatedData,
     currency: currency,
-    stacked: stacked,
-    data: state.data,
+    stacked: stacked // data={state.data}
+    ,
     gradient: gradient,
     fillColors: fillColors,
     getColorScheme: getColorScheme,
